e9fbb617e85c9037a20572d457db48ee465b4014
thermo-jam <nikamenskii@gmail.com>
2026-05-29 21:44:54 +0300

    feat(sections): Map

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from '@/components/sections/Hero';
 import { Problem } from '@/components/sections/Problem';
+import { Map } from '@/components/sections/Map';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Problem />
+      <Map />
     </>
   );
 }
