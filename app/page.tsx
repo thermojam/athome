41164977940d80d955a36759c70c4afb9b410920
thermo-jam <nikamenskii@gmail.com>
2026-05-29 21:43:32 +0300

    feat(sections): Problem

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/components/sections/Hero';
+import { Problem } from '@/components/sections/Problem';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Problem />
     </>
   );
 }
